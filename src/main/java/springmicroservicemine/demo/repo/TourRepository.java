425cca7aad30780f144e8d654a44806594ddb95e
Anthony Doban <doban911@aol.com>
2021-12-22 16:26:04 -0500

    workingPager

--- src/main/java/springmicroservicemine/demo/repo/TourRepository.java
+++ src/main/java/springmicroservicemine/demo/repo/TourRepository.java
@@ -1,11 +1,14 @@
 package springmicroservicemine.demo.repo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import springmicroservicemine.demo.domain.Tour;
 
 import java.util.List;
 
 public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> { //type of id or tour is integer
-    List<Tour> findByTourPackageCode(String code);
+    Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
 }
